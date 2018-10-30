1e85d99bf449b097e2d896c571b53d3a06bb5f21
Javierh98 <hjavier4@hotmail.com>
2018-10-30 10:40:38 -0600

    c

--- ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ProyectoFinal.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Más información al alcance de su mano.";
 
             return View();
         }
