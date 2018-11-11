11715b04d47a01aed989717e121a10fe5cd3d2fd
Javierh98 <hjavier4@hotmail.com>
2018-11-10 20:52:53 -0600

    p

--- ProyectoFinal/Controllers/EmailSetupController.cs
+++ ProyectoFinal/Controllers/EmailSetupController.cs
@@ -6,6 +6,10 @@ using System.Web.Mvc;
 using ProyectoFinal.Models;
 using System.Net;
 using System.Net.Mail;
+using SRVTextToImage;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace ProyectoFinal.Controllers
 {
@@ -20,22 +24,46 @@ namespace ProyectoFinal.Controllers
         [HttpPost]
         public ActionResult eMail(ProyectoFinal.Models.gmail model)
         {
-            MailMessage mm = new MailMessage("hjavier498@gmail.com", model.To);
-            mm.Subject = model.Subject;
-            mm.Body = model.Body;
-            mm.IsBodyHtml = false;
+            ModelState.Clear();
 
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = "smtp.gmail.com";
-            smtp.Port = 587;
-            smtp.EnableSsl = true;
+            if (this.Session["CaptchaImageText"].ToString() == Request.Form["CaptchaText"].ToString())
+            {
+                MailMessage mm = new MailMessage("hjavier498@gmail.com", model.To);
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                mm.IsBodyHtml = false;
 
-            NetworkCredential nc = new NetworkCredential("javierhernandezaproyecto@gmail.com", "emailproyecto");
-            smtp.UseDefaultCredentials = true;
-            smtp.Credentials = nc;
-            smtp.Send(mm);
-            ViewBag.Message = "Se envio correctamente";
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+
+                NetworkCredential nc = new NetworkCredential("javierhernandezaproyecto@gmail.com", "emailproyecto");
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = nc;
+                smtp.Send(mm);
+
+                ViewBag.Message = "Captcha Validation success!";
+            }
+            else
+            {
+                ViewBag.Message = "Captcha Validation Failed!";
+                return View(model);
+            }
             return View();
+        }
+
+        [HttpGet]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
+        public FileResult GetCaptchaImage()
+        {
+            CaptchaRandomImage CI = new CaptchaRandomImage();
+            this.Session["CaptchaImageText"] = CI.GetRandomString(10);
+            CI.GenerateImage(this.Session["CaptchaImageText"].ToString(), 300, 75, Color.DarkGray, Color.White);
+            MemoryStream stream = new MemoryStream();
+            CI.Image.Save(stream, ImageFormat.Png);
+            stream.Seek(0, SeekOrigin.Begin);
+            return new FileStreamResult(stream, "image/png");
         }
     } 
 }
